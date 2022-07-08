27a2e24f3ff07cb37c7964d9e52ae9208270ff17
Vajmir <sassiffy1004@gmail.com>
2022-07-08 14:40:12 +0200

    ⚙️ Some fixes

--- test/math/utils/LCM.test.ts
+++ test/math/utils/LCM.test.ts
@@ -1,23 +1,23 @@
 import { LCM } from "./../../../src"
 
-describe("Least common multiple", () => {
-  test("Least common multiple - 1", () => {
+describe("Lowest common multiple", () => {
+  test("Lowest common multiple - 1", () => {
     expect(LCM(10, 5)).toBe(10)
   })
 
-  test("Least common multiple - 2", () => {
+  test("Lowest common multiple - 2", () => {
     expect(LCM(10, 7)).toBe(70)
   })
 
-  test("Least common multiple - 3", () => {
+  test("Lowest common multiple - 3", () => {
     expect(LCM(15240, 552)).toBe(350520)
   })
 
-  test("Least common multiple - 4", () => {
+  test("Lowest common multiple - 4", () => {
     expect(LCM(1023, 5523)).toBe(1883343)
   })
 
-  test("Least common multiple - 5", () => {
+  test("Lowest common multiple - 5", () => {
     expect(LCM(10, 534)).toBe(2670)
   })
 })
